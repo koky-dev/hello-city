d89c458b2de16da7fc080159ea1374dad4d570ee
koky-php <dramekoky@gmail.com>
2021-07-02 13:18:04 +0200

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,7 @@
     About me | {{ config('app.name') }}
 @endsection
 @section('content')
+<img src="/images/me.jpeg" alt="">
     <p>Buit with &hearts; by Koky Dramé with the help of Les Teachers Du Net</p>
     <p><a href="/">Revenir à la page d'accueil</a></p>
 
